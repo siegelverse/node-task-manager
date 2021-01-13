2f2af22c9af1bed4fc437ac1687e8aded265168e
siegelverse <msiegel37@gmail.com>
2021-01-13 12:30:41 -0600

    restructure user model & create user post path

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,45 +6,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Must provide valid email')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
 
-    },
-    password: {
-        type: String,
-        required: true, 
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('"password" cannot be used as password')
-            }
-        }
-    }
-})
 
 const Task = mongoose.model('Task', {
     description: {
