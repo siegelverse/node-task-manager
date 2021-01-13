1b67ce414f182c7a1be2ef134ba583a5801b0dbe
siegelverse <msiegel37@gmail.com>
2021-01-12 18:32:29 -0600

    create user model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,3 +5,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
