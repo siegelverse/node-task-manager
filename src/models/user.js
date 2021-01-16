d9cff1a1b6acad69b46a6caf7aadc061e45e2784
siegelverse <msiegel37@gmail.com>
2021-01-16 17:34:31 -0600

    add jwt to user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -13,6 +14,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         trim: true,
         lowercase: true,
+        unique: true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Must provide valid email')
@@ -39,9 +41,41 @@ const userSchema = new mongoose.Schema({
                 throw new Error('"password" cannot be used as password')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String, 
+            required: true
+        }
+    }]
 })
 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user.id.toString() }, 'thisisasecr3t')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+    return user
+}
+
+//hash plaintext to password
 userSchema.pre('save', async function (next) {
     const user = this
 
