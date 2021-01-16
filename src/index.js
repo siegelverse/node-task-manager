33170a0769750b809cd613d578180686dd04fb5e
siegelverse <msiegel37@gmail.com>
2021-01-15 20:06:24 -0600

    patch functionality for models

--- src/index.js
+++ src/index.js
@@ -44,10 +44,31 @@ app.post('/users', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid updates!' })
+    }
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
 app.get('/tasks', async (req, res) => {
 
     try {
-        const tasks = Task.find({})
+        const tasks = await Task.find({})
         res.send(tasks)
     } catch(e) {
         res.status(500).send()
@@ -78,6 +99,27 @@ app.post('/tasks', async (req, res) => {
         res.status(201).send(task)
     } catch(e) {
         res.status(400).send(error)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid updates!' })
+    }
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(e) {
+        res.status(400).send(e)
     }
 })
 
